7bcdac8b41860f0debb04cd8b476953a525d1dad
Yuki Izumi <yuki@kivikakk.ee>
2015-05-30 14:05:36 +1000

    Typematic implementation.
    Only just realised SDL has its own! (and we hereby ignore it
    explicitly.)

--- main.c
+++ main.c
@@ -21,7 +21,10 @@ int main(int argc, char **argv) {
 
 #define FPS 60  /* Technically: vsync rate */
 #define TICK_LENGTH (1000 / FPS)
-#define FLIP_CURSOR 16
+#define FLIP_CURSOR 16  /* frame count */
+
+#define TYPEMATIC_DELAY 250
+#define TYPEMATIC_REPEAT 100
 
 void loop(void) {
     SDL_Event event;
@@ -30,17 +33,38 @@ void loop(void) {
     text_refresh();
     int until_flip = FLIP_CURSOR;
 
+    Uint32 keydown_tick = 0;
+    SDL_Keycode keydown_sym;
+    int typematic_on = 0;
+
     while (qb_running) {
+        if (keydown_tick && !typematic_on && last_tick >= keydown_tick + TYPEMATIC_DELAY) {
+            typematic_on = 1;
+            keydown_tick = last_tick;
+            qb_keypress(keydown_sym);
+        } else if (keydown_tick && typematic_on && last_tick >= keydown_tick + TYPEMATIC_REPEAT) {
+            keydown_tick = last_tick;
+            qb_keypress(keydown_sym);
+        }
+
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
                     return;
 
                 case SDL_KEYDOWN:
-                case SDL_KEYUP:
-                    if (event.key.state == SDL_PRESSED) {
-                        qb_keypress(event.key.keysym.sym);
+                    if (event.key.repeat) {
+                        break;
                     }
+
+                    qb_keypress(event.key.keysym.sym);
+                    keydown_tick = SDL_GetTicks();
+                    keydown_sym = event.key.keysym.sym;
+                    typematic_on = 0;
+                    break;
+
+                case SDL_KEYUP:
+                    keydown_tick = 0;
                     break;
             }
         }
